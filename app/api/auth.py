c547adb2abf3317acbfc9e874097306d4d4b9bc2
patrickcap <62596801+patrickcap@users.noreply.github.com>
2023-10-18 09:30:28 +0100

    removed comment

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,11 +1,14 @@
+# 
+
+import os
+
 from fastapi import status, Security, HTTPException
 from fastapi.security import APIKeyHeader
 
 # API key for private endpoints that require a valid API key
-api_keys = ["my_api_key"]
 api_key_header = APIKeyHeader(name="X-api-key", auto_error=False)
 def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
-    if api_key_header in api_keys:
+    if os.environ.get('MY_API_KEY') == api_key_header:
         return api_key_header
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
