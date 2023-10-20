42e0a91f0a190f8063ce39051086df9364407212
Kaixinwang99 <1006431552kaixin@gmail.com>
2023-10-20 10:32:57 +0100

    training function

--- model_file/main.py
+++ model_file/main.py
@@ -1,49 +1,16 @@
-#import soupport file
-from data_processing import *
-from model_setting import *
-
 import os
-#import pipeline related lib
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.pipeline import Pipeline
+from train_model import train_model
+#get_file_path
+csv_path = os.path.join(os.getcwd(),'data.csv')
+
+#train and save model
+train_model(csv_path,"test1")
+'''
 #import perfromance evaluation library
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import recall_score, make_scorer, confusion_matrix, roc_curve, auc, accuracy_score, f1_score, roc_auc_score
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
-
-#load and pre-process data
-csv_path = os.path.join(os.getcwd(),'data.csv')
-df = process_add_columns(drop_columns(compute_delay(load_data(csv_path))))
-
-#split test and train data set
-train_data,train_labels,test_data,test_labels = split_dataset(df)
-
-#transformer
-transformer = ColumnTransformer(transformers=[
-    ('tnf1',OrdinalEncoder(categories=[['morning','afternoon','evening','night'],['N','I']]),['part_of_day','flight_type']),
-    ('tnf2',OneHotEncoder(handle_unknown='ignore',sparse=False,drop='first'),['sched_destination_city_code','sched_airlinecode','is_weekend','sched_flight_month'])
-],remainder='passthrough')
-
-#pipeline
-#please add new model in model_setting.py and just change the model fucntion in pipeline
-XGBCV_pipeline = Pipeline([
-    ('transformer', transformer),
-    ('XB_boosting', GridXGB)
-    ])
-
-# Train it and print the best parameters (3 pts)
-XGBCV_pipeline.fit(train_data, train_labels)
-
-print(XGBCV_pipeline['XB_boosting'].best_params_)
-XGB = XGBCV_pipeline['XB_boosting'].best_estimator_
-#shows performance
-XGB_pipeline = Pipeline([
-    ('transformer', transformer),
-    ('XB_boosting', XGB)
-    ])
 
 cv = cross_val_score(XGB_pipeline, train_data, train_labels, cv=5, scoring='accuracy', error_score='raise').mean()
 print('CV Accuracy Score: ', str(cv))
@@ -77,3 +44,5 @@ plt.xlabel(r'False Positive Rate')
 plt.ylabel("True Positive Rate")
 plt.title("ROC Curve")
 plt.show()
+
+'''
