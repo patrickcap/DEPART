0a1c4e96f0707194277c25433a6d24683fbd4648
Kaixinwang99 <1006431552kaixin@gmail.com>
2023-10-24 12:46:30 +0100

    half way to train the model

--- app/api/operations/create_model.py
+++ app/api/operations/create_model.py
@@ -8,17 +8,27 @@ from fastapi import APIRouter
 
 from api.resources.model import ModelParams, Model, models
 from api.resources.enumerations import ModelStatus
+#get training model file
+from model_file import train_model
+from fastapi import File, UploadFile
+import shutil
 
 # Provides a reference to this endpoint for use by main FastAPI object
 create_model_router = APIRouter(prefix='/models')
 
+@create_model_router.post("/uploader/")
+async def create_upload_file(file: UploadFile = File(...)):
+   with open("destination.png", "wb") as buffer:
+      shutil.copyfileobj(file.file, buffer)
+   return {"filename": file.filename}
+
 # Using a POST request to submit model creation information from the user
-@create_model_router.post("", status_code=200)
-async def create_model(new_model: ModelParams):
+@create_model_router.post("/training")
+async def create_model(data_file:str):
     """
     Use the user-specified parameters to create, train, and store a model. Return a model identifier and status.
     """
     # Create unique id for this model, set status to pending and copy model parameters to new completed model object, then add to list
-    model = Model(id=uuid.uuid4(), status=ModelStatus.PENDING, params=new_model)
+    model = train_model(data_file,uuid.uuid3())
     models.append(model)
     return {"message": "New model (" + str(model.id) + ") has status " + str(model.status) + "."}
---
+++ app/model_file/main.py
@@ -0,0 +1,48 @@
+import os
+from train_model import train_model
+
+#load and pre-process data
+csv_path = os.path.join(os.getcwd(),'data.csv')
+
+# train and save model
+train_model(csv_path,'test1')
+'''
+#import perfromance evaluation library
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import recall_score, make_scorer, confusion_matrix, roc_curve, auc, accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+cv = cross_val_score(XGB_pipeline, train_data, train_labels, cv=5, scoring='accuracy', error_score='raise').mean()
+print('CV Accuracy Score: ', str(cv))
+
+pred_test_labels = XGB_pipeline.fit(train_data, train_labels).predict(test_data)
+
+accuracy = accuracy_score(pred_test_labels, test_labels)
+f1 = f1_score(pred_test_labels, test_labels, average="weighted")
+print('Accuracy Score: ', str(accuracy))
+print('f1 Score: ', str(f1))
+
+cm = confusion_matrix(test_labels, pred_test_labels)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Delayed", "On Time"])
+disp.plot()
+
+
+prob_test_labels = XGB_pipeline.fit(train_data, train_labels).predict_proba(test_data)
+prob_test_labels = prob_test_labels[:, 1]
+
+# Calculate the ROC curve points
+fpr, tpr, thresholds = roc_curve(test_labels,prob_test_labels)
+
+# Save the AUC in a variable to display it
+auc = np.round(roc_auc_score(y_true = test_labels, y_score = prob_test_labels), decimals = 3)
+
+# Create and show the plot
+
+plt.plot(fpr,tpr,label="AUC = " + str(auc))
+plt.legend(loc=4)
+plt.xlabel(r'False Positive Rate')
+plt.ylabel("True Positive Rate")
+plt.title("ROC Curve")
+plt.show()
+'''
