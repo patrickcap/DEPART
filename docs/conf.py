3e5c22905a8a6650b2281891fd1ca3543e38f4f6
patrickcap <62596801+patrickcap@users.noreply.github.com>
2023-10-27 07:35:53 +0100

    removed default text from docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,3 +26,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+# Prevent "Built with Sphinx using a theme provided by Read the Docs." from showing
+html_show_sphinx = False
