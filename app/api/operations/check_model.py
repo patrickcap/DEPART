0282cab68c2ab4fe09c823732f00de63e7e9780f
patrickcap <62596801+patrickcap@users.noreply.github.com>
2023-10-30 11:30:11 +0000

    fixed pylint issue using .pylintrc file

--- app/api/operations/check_model.py
+++ app/api/operations/check_model.py
@@ -15,7 +15,8 @@ check_model_router = APIRouter(prefix='/models/{model_id}')
 @check_model_router.get("", status_code=200)
 async def check_model(model_id: uuid.UUID, export: bool = False):
     """
-    Use the user-specified model ID to find the model in the store and export the model if required. Return the status of the model.
+    Use the user-specified model ID to find the model in the store and export the 
+    model if required. Return the status of the model.
     """
     print(export)
 
--- model/train_model.py
+++ model/train_model.py
@@ -1,15 +1,19 @@
-#import soupport file
+"""
+Helper function to train machine learning model
+"""
+
+import pickle
 from data_processing import *
 from model_setting import *
-#import pipeline related lib
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.pipeline import Pipeline
-import pickle
-
 
 def train_model(file_path,filename):
+    """
+    Train the machine learning model
+    """
     df = process_add_columns(drop_columns(compute_delay(load_data(file_path))))
 
     #split test and train data set
@@ -17,23 +21,25 @@ def train_model(file_path,filename):
 
     #transformer
     transformer = ColumnTransformer(transformers=[
-        ('tnf1',OrdinalEncoder(categories=[['morning','afternoon','evening','night'],['N','I']]),['part_of_day','flight_type']),
-        ('tnf2',OneHotEncoder(handle_unknown='ignore',sparse=False,drop='first'),['sched_destination_city_code','sched_airlinecode','is_weekend','sched_flight_month'])
+        ('tnf1',OrdinalEncoder(categories=[['morning','afternoon','evening','night'],
+                                           ['N','I']]),['part_of_day','flight_type']),
+        ('tnf2',OneHotEncoder(handle_unknown='ignore',sparse=False,drop='first'),
+         ['sched_destination_city_code','sched_airlinecode','is_weekend','sched_flight_month'])
     ],remainder='passthrough')
 
     #pipeline
     #please add new model in model_setting.py and just change the model fucntion in pipeline
-    Grid_pipeline = Pipeline([
+    grid_pipeline = Pipeline([
         ('transformer', transformer),
         ('XB_boosting', GridXGB)
         ])
     # Train it and print the best parameters (3 pts)
-    Grid_pipeline.fit(train_data, train_labels)
-    XGB = Grid_pipeline['XB_boosting'].best_estimator_
+    grid_pipeline.fit(train_data, train_labels)
+    xgb = grid_pipeline['XB_boosting'].best_estimator_
     #shows performance
     pipeline = Pipeline([
         ('transformer', transformer),
-        ('XB_boosting', XGB)
+        ('XB_boosting', xgb)
         ])
-    
+
     pickle.dump(pipeline, open(filename, 'wb'))
